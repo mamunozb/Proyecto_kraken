815260951ad40817606552a4427ee0d78df738a6
mamunozb <ma.munozb1@uniandes.edu.co>
2023-05-06 11:16:08 -0500

    Pruebas tag

--- features/my_first.feature
+++ features/my_first.feature
@@ -1,16 +1,207 @@
-Feature: Iniciar una conversación
+Feature: Pruebas ghost
 
 @user1 @web
-Scenario: Como primer usuario inicio sesion y mandó un mensaje al usuario 2
+Scenario: Creación de tag
   Given I navigate to page "http://localhost:2368/ghost"
   And I wait for 2 seconds
   When I enter email "<USERNAME1>"
   And I wait for 2 seconds
   And I enter password "<PASSWORD1>"
   And I wait for 2 seconds
-  And I click next
-  And I wait for 7 seconds
-  And I click options
+  And I click sign in
+  And I wait for 10 seconds
+  And I click tag
+  And I wait for 2 seconds
+  And I click new tag
+  And I wait for 2 seconds
+  When I enter name tag "<TAG1>"
+  And I wait for 2 seconds
+  And I click save
+  And I wait for 2 seconds
+  Then I validate title tag "<TAG1>"
+  And I wait for 2 seconds
+  When I click tag
+  And I wait for 2 seconds
+  And I click additional
+  And I wait for 2 seconds
+  And I click sign out
+  And I wait for 2 seconds
+
+@user2 @web
+Scenario: Modificación de tag campo nombre
+  Given I navigate to page "http://localhost:2368/ghost"
+  And I wait for 2 seconds
+  When I enter email "<USERNAME1>"
+  And I wait for 2 seconds
+  And I enter password "<PASSWORD1>"
+  And I wait for 2 seconds
+  And I click sign in
+  And I wait for 12 seconds
+  And I click tag
+  And I wait for 2 seconds
+  And I click new tag
+  And I wait for 2 seconds
+  When I enter name tag "<TAG2>"
+  And I wait for 2 seconds
+  And I click save
+  And I wait for 5 seconds
+  Then I validate title tag "<TAG2>"
+  When I wait for 2 seconds
+  And I get text value slug
+  And I wait for 2 seconds
+  And I click tag
+  And I wait for 2 seconds
+  And I click tag list
+  And I enter name tag "<TAG3>"
+  And I wait for 2 seconds
+  And I click save
+  And I wait for 2 seconds
+  Then I validate title tag "<TAG3>"
+  When I wait for 2 seconds
+  And I click tag
+  And I wait for 2 seconds
+  And I click additional
+  And I wait for 2 seconds
+  And I click sign out
+  And I wait for 2 seconds
+
+@user3 @web
+Scenario: Borrar tag
+  Given I navigate to page "http://localhost:2368/ghost"
+  And I wait for 2 seconds
+  When I enter email "<USERNAME1>"
+  And I wait for 2 seconds
+  And I enter password "<PASSWORD1>"
+  And I wait for 2 seconds
+  And I click sign in
+  And I wait for 14 seconds
+  And I click tag
+  And I wait for 2 seconds
+  And I click new tag
+  And I wait for 2 seconds
+  When I enter name tag "<TAG2>"
+  And I wait for 2 seconds
+  And I click save
+  And I wait for 5 seconds
+  Then I validate title tag "<TAG2>"
+  When I wait for 2 seconds
+  And I get text value slug
+  And I wait for 2 seconds
+  And I click tag
+  And I wait for 2 seconds
+  And I click tag list
+  And I wait for 2 seconds
+  And I click delete tag
+  And I wait for 2 seconds
+  And I click confirm delete
+  And I wait for 2 seconds
+  Then I search tag
+  When I wait for 2 seconds
+  And I click additional
+  And I wait for 2 seconds
+  And I click sign out
+  And I wait for 2 seconds
+
+@user4 @web
+Scenario: Asignar tag a un post
+  Given I navigate to page "http://localhost:2368/ghost"
+  And I wait for 2 seconds
+  When I enter email "<USERNAME1>"
+  And I wait for 2 seconds
+  And I enter password "<PASSWORD1>"
+  And I wait for 2 seconds
+  And I click sign in
+  And I wait for 15 seconds
+  And I click tag
+  And I wait for 2 seconds
+  And I click new tag
+  And I wait for 2 seconds
+  When I enter name tag "<TAGPOST>"
+  And I wait for 2 seconds
+  And I click save
+  And I wait for 5 seconds
+  Then I validate title tag "<TAGPOST>"
+  When I wait for 2 seconds
+  And I get text value slug
+  And I wait for 2 seconds
+  And I click post
+  And I wait for 2 seconds
+  And I click new post
+  And I wait for 2 seconds
+  And I enter name post "<POST1>"
+  And I wait for 2 seconds
+  And I enter content post "<CONPOST1>"
+  And I wait for 2 seconds
+  And I click post
+  And I wait for 2 seconds
+  Then I search post "<POST1>"
+  And I wait for 2 seconds
+  When I click settings post
+  And I wait for 2 seconds
+  And I assing tag to post "<TAGPOST>"
+  And I wait for 2 seconds
+  And I close settings post
+  And I wait for 2 seconds
+  And I click update post
+  And I wait for 2 seconds
+  And I click confirm update post
+  Then I validate post publication
+  When I wait for 2 seconds
+  And I click post
+  When I wait for 10 seconds
+  And I click additional
+  And I wait for 2 seconds
+  And I click sign out
+  And I wait for 2 seconds
+
+@user5 @web
+Scenario: Asignar tag a una pagina
+  Given I navigate to page "http://localhost:2368/ghost"
+  And I wait for 2 seconds
+  When I enter email "<USERNAME1>"
+  And I wait for 2 seconds
+  And I enter password "<PASSWORD1>"
+  And I wait for 2 seconds
+  And I click sign in
+  And I wait for 16 seconds
+  And I click tag
+  And I wait for 2 seconds
+  And I click new tag
+  And I wait for 2 seconds
+  When I enter name tag "<TAGPAGE>"
+  And I wait for 2 seconds
+  And I click save
+  And I wait for 5 seconds
+  Then I validate title tag "<TAGPAGE>"
+  When I wait for 2 seconds
+  And I get text value slug
+  And I wait for 2 seconds
+  And I click page
+  And I wait for 2 seconds
+  And I click new page
+  And I wait for 2 seconds
+  And I enter name page "<PAGE1>"
+  And I wait for 2 seconds
+  And I enter content page "<CONPAGE1>"
+  And I wait for 2 seconds
+  And I click page
+  And I wait for 2 seconds
+  Then I search page "<PAGE1>"
+  And I wait for 2 seconds
+  When I click settings page
+  And I wait for 2 seconds
+  And I assing tag to post "<TAGPAGE>"
+  And I wait for 2 seconds
+  And I close settings page
+  And I wait for 2 seconds
+  And I click update page
+  And I wait for 2 seconds
+  And I click confirm update page
+  Then I validate page publication
+  When I wait for 2 seconds
+  And I click page
+  When I wait for 7 seconds
+  And I click additional
   And I wait for 2 seconds
   And I click sign out
   And I wait for 2 seconds
--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -1,36 +1,241 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+const pass = 'input[name="password"]';
+const identification = 'input[name="identification"]';
+const assert = require('assert');
+let slug;
+let urlpost;
 
 When('I enter email {kraken-string}', async function (email) {
-    let element = await this.driver.$('input[name="identification"]');
+    let element = await this.driver.$(identification);
     return await element.setValue(email);
 });
 
+
 When('I enter password {kraken-string}', async function (password) {
-    let element = await this.driver.$('input[name="password"]');
+    let element = await this.driver.$(pass);
     return await element.setValue(password);
 });
 
-When('I click next', async function() {
-    let element = await this.driver.$('#ember12');
+When('I click sign in', async function() {
+    let element = await this.driver.$('button[type="submit"]');
     return await element.click();
 });
 
-When('I click options', async function() {
-    let element = await this.driver.$('#ember47');
+When('I click additional', async function() {
+    let element = await this.driver.$('div[class="gh-nav-bottom"]');
     return await element.click();
 });
 
 When('I click sign out', async function () {
-    let element = await this.driver.$('ember136');
+    let element = await this.driver.$('a[href="#/signout/"]');
     return await element.click();
 });
 
-Then('I click on the redact message inputbox', async function () {
-    let element = await this.driver.$('p.xat24cr');
+When('I click tag', async function () {
+    let element = await this.driver.$('a[href="#/tags/"]');
     return await element.click();
-  });
+});
 
-  Then('I send the message', async function () {
-    let element = await this.driver.$('span.x3nfvp2:nth-child(3)');
+When('I click new tag', async function () {
+    let element = await this.driver.$('a[href="#/tags/new/"]');
     return await element.click();
+}); 
+
+When('I click save', async function () {
+    let element = await this.driver.$('span=Save');
+    return await element.click();
+}); 
+
+When('I enter name tag {kraken-string}', async function (tag) {
+    let element = await this.driver.$('input[name="name"]');
+    return await element.setValue(tag);
+});
+
+Then('I validate title tag {kraken-string}', async function (tag) {
+    let element = await this.driver.$('h2.gh-canvas-title');
+    let text = await element.getText();
+    return assert.equal(text, `Tags\n${tag}` );
+});
+
+When('I get text value slug', async function () {
+    let eleslug = await this.driver.$('input[name="slug"]');
+    this.slug = await eleslug.getValue();
+    return this.slug;
+});
+
+When('I click tag list', async function () {
+    let element = await this.driver.$(`a[href="#/tags/${this.slug}/"]`);
+    return await element.click();
+});
+
+When('I click delete tag', async function () {
+    let element = await this.driver.$('span=Delete tag');
+    return await element.click();
+});
+
+When('I click confirm delete', async function () {
+    let element = await this.driver.$('span=Delete');
+    return await element.click();
+});
+
+Then('I search tag', async function () {
+    try {
+        let element = await this.driver.$(`a[href="#/tags/${this.slug}/"]`);
+    } catch (error) {
+        return error;
+    }
+});
+
+When('I click post', async function () {
+    let element = await this.driver.$('a[href="#/posts/"]');
+    return await element.click();
+});
+
+When('I click new post', async function () {
+    let element = await this.driver.$('a[href="#/editor/post/"]');
+    return await element.click();
+}); 
+
+When('I enter name post {kraken-string}', async function (post) {
+    let element = await this.driver.$('textarea[placeholder="Post Title"]');
+    return await element.setValue(post);
+});
+
+When('I enter content post {kraken-string}', async function (contpost) {
+    let element = await this.driver.$('p[data-koenig-dnd-droppable="true"]');
+    await element.click();
+    return await element.setValue(contpost);
+});
+
+Then('I search post {kraken-string}', async function (post) {
+    try {
+        let element = await this.driver.$(`h3=${post}`);
+        return await element.click();
+    } catch (error) {
+        return error;
+    }
+});
+
+When('I click settings post', async function () {
+    let element = await this.driver.$('button[title="Settings"]');
+    return await element.click();
+}); 
+
+When('I assing tag to post {kraken-string}', async function (tag) {
+    let element = await this.driver.$('input[type="Search"]');
+    await element.click();
+    let eletag = await this.driver.$(`li=${tag}`);
+    return await eletag.click();
+}); 
+
+When('I close settings post', async function () {
+    let element = await this.driver.$('button[aria-label="Close"]');
+    return await element.click();
+});
+
+When('I click update post', async function () {
+    try {
+        let element = await this.driver.$('span=Udate');
+        return await element.click();
+    } catch (error) {
+        let element = await this.driver.$('span=Publish');
+        return await element.click();
+    }
+}); 
+
+
+When('I click confirm update post', async function () {
+    try {
+        let element = await this.driver.$("button[class='gh-btn gh-btn-blue gh-publishmenu-button gh-btn-icon ember-view']");
+        return await element.click();
+    } catch (error) {
+        element = await this.driver.$("button[class='gh-btn gh-btn-blue gh-publishmenu-button gh-btn-icon ember-view']");
+        return await element.click();
+    }
+}); 
+
+Then('I validate post publication', async function () {
+    try {
+        let element = await this.driver.$('span=Published');
+        return assert.equal("Publish", element.getText());
+    } catch (error) {
+        return error;
+    }
+});
+
+When('I click page', async function () {
+    let element = await this.driver.$('a[href="#/pages/"]');
+    return await element.click();
+});
+
+When('I click new page', async function () {
+    let element = await this.driver.$('a[href="#/editor/page/"]');
+    return await element.click();
+}); 
+
+When('I enter name page {kraken-string}', async function (page) {
+    let element = await this.driver.$('textarea[placeholder="Page Title"]');
+    return await element.setValue(page);
+});
+
+When('I enter content page {kraken-string}', async function (contpage) {
+    let element = await this.driver.$('p[data-koenig-dnd-droppable="true"]');
+    await element.click();
+    return await element.setValue(contpage);
+});
+
+Then('I search page {kraken-string}', async function (page) {
+    try {
+        let element = await this.driver.$(`h3=${page}`);
+        return await element.click();
+    } catch (error) {
+        return error;
+    }
+});
+
+When('I click settings page', async function () {
+    let element = await this.driver.$('button[title="Settings"]');
+    return await element.click();
+}); 
+
+When('I assing tag to page {kraken-string}', async function (tag) {
+    let element = await this.driver.$('input[type="Search"]');
+    await element.click();
+    let eletag = await this.driver.$(`li=${tag}`);
+    return await eletag.click();
+}); 
+
+When('I close settings page', async function () {
+    let element = await this.driver.$('button[aria-label="Close"]');
+    return await element.click();
+});
+
+When('I click update page', async function () {
+    try {
+        let element = await this.driver.$('span=Udate');
+        return await element.click();
+    } catch (error) {
+        let element = await this.driver.$('span=Publish');
+        return await element.click();
+    }
+}); 
+
+
+When('I click confirm update page', async function () {
+    try {
+        let element = await this.driver.$("button[class='gh-btn gh-btn-blue gh-publishmenu-button gh-btn-icon ember-view']");
+        return await element.click();
+    } catch (error) {
+        element = await this.driver.$("button[class='gh-btn gh-btn-blue gh-publishmenu-button gh-btn-icon ember-view']");
+        return await element.click();
+    }
+}); 
+
+Then('I validate page publication', async function () {
+    try {
+        let element = await this.driver.$('span=Published');
+        return assert.equal("Publish", element.getText());
+    } catch (error) {
+        return error;
+    }
 });
